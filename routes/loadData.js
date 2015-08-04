fdf70fbab2c6bfae40b3d195463e284952efc009
Ganesh Iyer <lastlegion@gmail.com>
2015-08-04 09:45:37 +0530

    server side summary statistics

--- routes/loadData.js
+++ routes/loadData.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs')
+var fs = require('fs');
+
+var dl = require('datalib');
+
 var dataSource = require("../modules/dataSource"),
     interactiveFilters = require("../modules/interactiveFilters");
 
@@ -12,22 +15,44 @@ router.get('/', function (req, res, next) {
 
     dataSource.loadData(function(data){
         console.log("....")
-        //  console.log(data);
+        console.log(data);
 
 
         var attributes = [];
+
+        var types = dl.type.inferAll(data);
+
+        var summary  = dl.summary(data);
+        var x = summary.map(function(attribute){
+
+            var stats = {
+                name: attribute.field,
+                type: attribute.type,
+                max: attribute.max,
+                min: attribute.min,
+                mean: attribute.mean,
+                stdev: attribute.stdev,
+                distinct: attribute.distinct
+            }
+            attributes.push(stats);
+        })
+        /*
         for(var key in data[0]){
-            attributes.push(key)
+            var attribute = {};
+            attribute.name = key;
+            attribute.type = types[key];
+            console.log(attribute)
+            attributes.push(attribute)
         }
+        */
 
         //Apply crossfilter on whole data
         interactiveFilters.applyCrossfilter(data);
         
-
-        console.log(attributes)
+        //console.log(attributes)
 
         res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({attributes: attributes}));
+        res.send(JSON.stringify(attributes));
 
 
 
